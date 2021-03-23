1190fec4dc25d4f6479f60c822fa800ff5b67e0d
popov.alexey <alexey.popov@devexpress.com>
2021-03-23 19:03:10 +0300

    feat: added first test

--- test/config-test.js
+++ test/config-test.js
@@ -1,3 +1,8 @@
-import { config } from '../src/config.js';
+import defaultConfig, { config, HOST, PORT } from '../src/config.js';
+import { expect } from 'chai';
+
+expect(HOST, 'HOST equal localhost').to.equal('localhost');
 
 console.log(config);
+console.log(HOST, PORT);
+console.log(defaultConfig);
