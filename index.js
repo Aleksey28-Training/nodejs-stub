61782f05efb7d61c1417fb8e7c290ad94e4bfe9f
popov.alexey <alexey.popov@devexpress.com>
2021-03-24 18:30:22 +0300

    feat: added possibility only exporting app server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import Koa from 'koa';
 import { PORT } from './src/config.js';
+import esMain from 'es-main';
+
 
 const app = new Koa();
 
@@ -7,4 +9,10 @@ app.use(async ctx => {
     ctx.body = 'Hello World';
 });
 
-app.listen(PORT);
+if (esMain(import.meta)) {
+    app.listen(PORT, async () => {
+        console.log(`Example app listening at http://localhost:${PORT}`);
+    });
+}
+
+export default app;
