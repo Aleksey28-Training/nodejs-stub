c55861fcf77d0ef3538b77ef59a23d18cfbbf538
popov.alexey <alexey.popov@devexpress.com>
2021-03-24 17:46:33 +0300

    feat: added server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// TODO: Start prototyping here
+import Koa from 'koa';
+import { PORT } from './src/config.js';
+
+const app = new Koa();
+
+app.use(async ctx => {
+    ctx.body = 'Hello World';
+});
+
+app.listen(PORT);
