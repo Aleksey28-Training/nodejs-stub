b3e45d7f9866c1a460ee84344591893ff66e8dc4
popov.alexey <alexey.popov@devexpress.com>
2021-03-25 18:00:16 +0300

    refactor: edited prefix in the instruction for turn on "debug", moved the values to constants

--- src/config.js
+++ src/config.js
@@ -1,13 +1,20 @@
 import { config } from 'dotenv';
 import debug from 'debug';
+import { name } from '../package.json';
 
 config();
 
-const debugConfig = debug('config');
-
 const { PORT = '1337', HOST = 'localhost' } = process.env;
 
-//HACK: You need to tap $env:DEBUG="config" in terminal to turn on debug
+const DEBUG_PREFIX_APP    = 'app';
+const DEBUG_PREFIX_CONFIG = 'config';
+
+const debugApp    = debug(DEBUG_PREFIX_APP);
+const debugConfig = debug(DEBUG_PREFIX_CONFIG);
+
+//NOTE: You need to tap $env:DEBUG="config" in terminal to turn on debug
+debugApp('booting %o', name);
+
 debugConfig(`PORT: ${PORT}`);
 debugConfig(`HOST: ${HOST}`);
 
