e09f88e1a80d93154010ad0eca4e2a2f1821e8b1
Aleksey28 <alekseypopow1995@yandex.ru>
2021-03-30 17:10:09 +0300

    fix: changed creating server

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-import { Config, debugApp, debugConfig } from './src/config.js';
+import { Config, debugApp } from './src/config.js';
 import esMain from 'es-main';
 import Server from './src/server.js';
-import { readFileSync } from 'fs';
 
-async function run () {
+const server = Server.create();
 
-    const config = new Config({ host: 'localhost', port: 1337 });
-
-    debugConfig('%O', config);
-
-    const { port } = Config.globals;
-    const packageJSON = JSON.parse(readFileSync('./package.json', 'utf8'));
-
-    //NOTE: You need to tap $env:DEBUG="app" in terminal to turn on debug
-    debugApp('%o booting', packageJSON.name);
-
-    const server = new Server(port);
-
-    if (esMain(import.meta)) {
+if (esMain(import.meta)) {
+    (async function () {
         await server.start();
-        debugApp(`%o is listening on port: ${port}`, packageJSON.name);
-    }
-
-    return server;
+        //NOTE: You need to tap $env:DEBUG="app" in terminal to turn on debug
+        debugApp(`App is listening on port: ${Config.port}`);
+    })();
 }
 
-export default run();
+export default server;
--- src/server.js
+++ src/server.js
@@ -3,12 +3,29 @@ import Koa from 'koa';
 import KoaStatic from 'koa-static';
 import path from 'path';
 import { once } from 'events';
+import { Config, debugApp, debugConfig } from './config.js';
+import { readFileSync } from 'fs';
 
 class Server {
     constructor (port) {
         this._port   = port;
         this._app    = new Koa();
         this._server = http.createServer(this._app.callback());
+    }
+
+    static create () {
+        const config = new Config({ host: 'localhost', port: 1337 });
+
+        //NOTE: You need to tap $env:DEBUG="config" in terminal to turn on debug
+        debugConfig('%O', config);
+
+        const { port } = Config.globals;
+        const packageJSON = JSON.parse(readFileSync('./package.json', 'utf8'));
+
+        //NOTE: You need to tap $env:DEBUG="app" in terminal to turn on debug
+        debugApp('%o booting', packageJSON.name);
+
+        return new Server(port);
     }
 
     async start () {
