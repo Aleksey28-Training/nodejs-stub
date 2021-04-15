27d4c25a4cfdc68487bda37e8b9c5cf66da7a6b5
Aleksey28 <alekseypopow1995@yandex.ru>
2021-04-15 12:17:25 +0300

    feat: added format token

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export class Config {
             host:  process.env['HOST'] || '',
             owner: process.env['OWNER'] || '',
             repo:  process.env['REPO'] || '',
-            token: process.env['GITHUB_TOKEN']
+            token: `Bearer ${process.env['GITHUB_TOKEN']}`
         };
     }
 
