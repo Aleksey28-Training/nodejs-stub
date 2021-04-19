0c780ee506368ebff547a7cf934c1a41029f51a3
Aleksey28 <alekseypopow1995@yandex.ru>
2021-04-19 15:47:06 +0300

    test: added changing status after rerun

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,16 +1,34 @@
-async function rerun (id: number): Promise<void> {
+const allButtonsRerun = document.querySelectorAll('.table__button');
+
+allButtonsRerun.forEach(item => {
+    item.addEventListener('click', rerun);
+});
+
+async function rerun (evt: Event): Promise<void> {
+
+    // const row = evt.target?.closest('tr')
+    if (!evt.target) {
+        return;
+    }
+
+    const row = (<HTMLButtonElement>evt.target).closest('tr');
+    const id = row?.querySelector('.table__column_name_id');
+    const status = row?.querySelector('.table__column_name_status');
+
     const options = {
         method:  'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ id })
+        body:    JSON.stringify({ id: id?.innerHTML })
     };
 
     try {
         await fetch('http://localhost:1337/rerun', options);
-    }
-    catch (error) {
+        if (status) {
+            status.innerHTML = 'queued';
+        }
+    } catch (error) {
         console.log(error);
     }
 }
