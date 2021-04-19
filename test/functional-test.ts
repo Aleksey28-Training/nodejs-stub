c606e9ae44e0984751404643226ec0a2304de8f5
Aleksey28 <alekseypopow1995@yandex.ru>
2021-04-19 18:41:50 +0300

    test: added functional test for rerun

--- test/functional-test.ts
+++ test/functional-test.ts
@@ -1,9 +1,26 @@
 import { Selector } from 'testcafe';
+import nock = require('nock');
+import { Config } from '../src/server/config';
 
-fixture `Getting Started`
-    .page `http://localhost:1337/`;
+fixture`Getting Started`
+    .page`http://localhost:1337/`;
 
 test('Rerun', async t => {
+
+    const config = new Config({ host: 'localhost', port: 1337 });
+
+    const { owner, repo, baseUrlGitHub = '' } = config.values;
+
+    const request = new RegExp(`/repos/${owner}/${repo}/actions/runs/\\d*/rerun`);
+
+    nock(baseUrlGitHub)
+        .post(request)
+        .reply(201, {});
+
     await t
         .click('.table__button');
+
+    const status = await Selector('.table__button').parent('tr').find('.table__column_name_status').innerText;
+
+    await t.expect(status).eql('queued');
 });
