c8c267ceaff7dcc232d93b1934522d8e5edc15e5
Aleksey28 <alekseypopow1995@yandex.ru>
2021-08-09 17:38:16 +0300

    pref: reorganized config and server

--- src/server/globals.ts
+++ src/server/globals.ts
@@ -1,5 +1,19 @@
-import { debugGlobal } from './config';
+import debug from 'debug';
 
+const DEBUG_PREFIX = 'node-stub';
+const DEBUG_PREFIX_APP = `${DEBUG_PREFIX}:app`;
+const DEBUG_PREFIX_CONFIG = `${DEBUG_PREFIX}:config`;
+const DEBUG_PREFIX_GLOBAL = `${DEBUG_PREFIX}:global`;
+const DEBUG_PREFIX_GITHUB_API = `${DEBUG_PREFIX}:github_api`;
+const DEBUG_PREFIX_RUNS = `${DEBUG_PREFIX}:runs`;
+const DEBUG_PREFIX_FUNCTIONAL_TEST = `${DEBUG_PREFIX}:ftest`;
+
+export const debugApp = debug(DEBUG_PREFIX_APP);
+export const debugConfig = debug(DEBUG_PREFIX_CONFIG);
+export const debugGlobal = debug(DEBUG_PREFIX_GLOBAL);
+export const debugApiGithub = debug(DEBUG_PREFIX_GITHUB_API);
+export const debugRuns = debug(DEBUG_PREFIX_RUNS);
+export const debugFunctionalTest = debug(DEBUG_PREFIX_FUNCTIONAL_TEST);
 
 export let baseUrlGitHub = 'https://api.github.com';
 export let host = 'localhost';
@@ -8,20 +22,31 @@ export let owner = '';
 export let repo = '';
 export let token = '';
 
-interface UpdateInterface {
-    port: number,
-    host: string,
-    owner: string,
-    repo: string,
-    token?: string,
-    baseUrlGitHub?: string,
-    [Symbol.toStringTag]?: string,
+export interface ValuesInterface {
+    owner: string;
+    repo: string;
+
+    port?: number;
+    host?: string;
+    token?: string;
+    baseUrlGitHub?: string;
+
+    // HACK: have to declare Symbol.toStringTag as optional since TS cannot deduct types even by well-known Symbols
+    [Symbol.toStringTag]?: string;
 }
 
-export function update (this: UpdateInterface, newValues: UpdateInterface): UpdateInterface {
+interface Module extends ValuesInterface {
+    [Symbol.toStringTag]: string;
+}
+
+function isModule (config: Module | ValuesInterface): config is Module {
+    return Symbol.toStringTag in config && config[Symbol.toStringTag] === 'Module';
+}
+
+export function update (this: Module | ValuesInterface, newValues: ValuesInterface): ValuesInterface {
     debugGlobal(typeof this);
 
-    if (this && !(this[Symbol.toStringTag] === 'Module'))
+    if (this && isModule(this))
         return Object.assign(this, newValues);
 
     host = newValues.host ? newValues.host : host;
